8a80816ab4c1bae31e020fafbd8750450474c47f
gikerix <vokinvezhok@gmail.com>
2017-02-23 22:22:28 -0500

    Roles management is added

---
+++ PhotoBank/src/PhotoBank/Controllers/RolesController.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using PhotoBank.Models;
+using PhotoBank.ViewModels;
+
+namespace PhotoBank.Controllers
+{
+    public class RolesController : Controller
+    {
+        RoleManager<IdentityRole> roleManager;
+        UserManager<User> userManager;
+
+        public RolesController(RoleManager<IdentityRole> RoleManager, UserManager<User> UserManager)
+        {
+            roleManager = RoleManager;
+            userManager = UserManager;
+        }
+
+        public IActionResult Index() => View(roleManager.Roles.ToList());
+
+        public IActionResult Create() => View();
+
+        [HttpPost]
+        public async Task<IActionResult> Create(string name)
+        {
+            if (!string.IsNullOrEmpty(name))
+            {
+                IdentityResult result = await roleManager.CreateAsync(new IdentityRole(name));
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    foreach (var error in result.Errors)
+                    {
+                        ModelState.AddModelError(string.Empty, error.Description);
+                    }
+                }
+            }
+            return View(name);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Delete(string id)
+        {
+            IdentityRole role = await roleManager.FindByIdAsync(id);
+            if (role != null)
+            {
+                IdentityResult result = await roleManager.DeleteAsync(role);
+            }
+            return RedirectToAction("index");
+        }
+
+        public IActionResult UserList() => View(userManager.Users.ToList());
+
+        public async Task<IActionResult> Edit(string userID)
+        {
+            //get user
+            User user = await userManager.FindByIdAsync(userID);
+            if (user != null)
+            {
+                //get list of roles for the user
+                var userRoles = await userManager.GetRolesAsync(user);
+                var allRoles = roleManager.Roles.ToList();
+                ChangeRoleViewModel model = new ChangeRoleViewModel
+                {
+                    UserId = user.Id,
+                    UserEmail = user.Email,
+                    UserRoles = userRoles,
+                    AllRoles = allRoles
+                };
+                return View(model);
+            }
+            return NotFound();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(string userID, List<string> roles)
+        {
+            User user = await userManager.FindByIdAsync(userID);
+            if (user != null)
+            {    
+                var userRoles = await userManager.GetRolesAsync(user);
+                var allRoles = roleManager.Roles.ToList();
+                var addedRoles = roles.Except(userRoles);
+                var removedRoles = userRoles.Except(roles);
+                await userManager.AddToRolesAsync(user, addedRoles);
+                await userManager.RemoveFromRolesAsync(user, removedRoles);
+                return RedirectToAction("Index", "Users");
+            }
+            return NotFound();
+        }
+    }
+}
